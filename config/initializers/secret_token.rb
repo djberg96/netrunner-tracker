3f41d890b744e3b05015e9360e12903731b711b5
Daniel Berger <djberg96@gmail.com>
2013-01-25 11:32:53 -0700

    Bare rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NetrunnerTracker::Application.config.secret_token = '952c81d06c1d062a54cf39673d1a84a80bd1ba700b4032abc553343d037d6453114a910594c43b215ed821df414254720ea381483c59a2c7c222ffe52fc9cf75'
