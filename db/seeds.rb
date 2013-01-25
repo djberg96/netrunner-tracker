8db57fa3b4a692a8b75a75f0bf3ca167d4a6f867
Daniel Berger <djberg96@gmail.com>
2013-01-25 13:08:26 -0700

    Added Corporation seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@ runners = Runner.create([
   {:faction => 'Shaper',    :identity => 'Natural', :name => "Kate 'Mac' McCaffrey"},
   {:faction => 'Anarchist', :identity => 'G-mod',   :name => 'Noise'}
 ])
+
+corporations = Corporation.create([
+  {:faction => 'Haas-Bioroid',       :identity => 'Megacorp'},
+  {:faction => 'Weyland Consortium', :identity => 'Megacorp'},
+  {:faction => 'Jinteki',            :identity => 'Megacorp'},
+  {:faction => 'NBN',                :identity => 'Megacorp'},
+])
