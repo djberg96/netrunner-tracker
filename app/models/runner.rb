d77b7b898992ab52a0aba66f120278b581d76d65
Daniel Berger <djberg96@gmail.com>
2013-01-25 11:51:18 -0700

    Added some Runner validations.

--- app/models/runner.rb
+++ app/models/runner.rb
@@ -1,3 +1,17 @@
 class Runner < ActiveRecord::Base
   attr_accessible :faction, :identity, :name
+
+  validates :faction,
+    :presence  => true,
+    :format    => {:with => /\A[a-zA-Z]+\z/, :message => "only letters allowed"},
+    :inclusion => {
+      :in => %w[Anarchist Criminal Shaper],
+      :message => 'must be "Anarchist", "Criminal" or "Shaper"'
+    }
+
+  validates :identity,
+    :presence => true
+
+  validates :name,
+    :presence => true
 end
