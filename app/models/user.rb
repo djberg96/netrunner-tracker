f3b17d6da00b290eeb68de8d3c32f491a8931977
Daniel Berger <djberg96@gmail.com>
2013-05-08 16:16:03 -0600

    Updated user password handling and validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :first_name, :last_name, :league_id, :userid
+  attr_accessible :password, :password_confirmation
+
   belongs_to :league
 
   has_secure_password
+
+  validates :password,
+    :presence => true,
+    :length   => {
+      :minimum  => 6,
+      :maximum  => 32
+    }
 end
