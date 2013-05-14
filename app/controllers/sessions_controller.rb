6c43d2dc37e66a23814961c1e32947f1ecfb2ebc
Daniel Berger <djberg96@gmail.com>
2013-05-14 15:42:55 -0600

    Authenticate users on login.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
 
-    if user
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to user_path(user.id)
     else
