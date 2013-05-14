e0340f35a5c2acd77529a9a4c1026b1abf6aa54d
Daniel Berger <djberg96@gmail.com>
2013-05-14 07:05:57 -0600

    Added validation for faction name to corporation model.

--- app/models/corporation.rb
+++ app/models/corporation.rb
@@ -1,10 +1,21 @@
 class Corporation < ActiveRecord::Base
   attr_accessible :faction, :identity, :slogan
 
+  FACTIONS = [
+    'Haas-Bioroid',
+    'Weyland Consortium',
+    'Jinteki',
+    'NBN'
+  ]
+
   has_many :games
 
   validates :faction,
-    :presence => true
+    :presence  => true,
+    :inclusion => {
+      :in => FACTIONS,
+      :message => 'must be ' + FACTIONS.join(', ')
+    }
 
   validates :identity,
     :presence => true
