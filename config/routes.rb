10fbe189779b4f11b8f43732691c29b8b3b5ad4c
Daniel Berger <djberg96@gmail.com>
2013-05-16 20:47:43 -0600

    Change default root to games index page and remove new link from users show view.

--- config/routes.rb
+++ config/routes.rb
@@ -92,7 +92,7 @@ NetrunnerTracker::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'users#index'
+  root :to => 'games#index'
 
   # See how all your routes lay out with "rake routes"
 
