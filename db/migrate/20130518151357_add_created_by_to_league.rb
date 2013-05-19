e0400708dd395a7a64b17fed55f761bd0c5a5e11
Daniel Berger <djberg96@gmail.com>
2013-05-19 09:55:01 -0600

    Updated created_by migration.

--- db/migrate/20130518151357_add_created_by_to_league.rb
+++ db/migrate/20130518151357_add_created_by_to_league.rb
@@ -1,8 +1,10 @@
 class AddCreatedByToLeague < ActiveRecord::Migration
   def change
     add_column :leagues, :created_by, :integer
+
     League.all.each{ |league|
-      league.update_attributes!(:created_by => User.find_by_userid('dannyboy').id)
+      league.created_by = User.find_by_userid('dannyboy')
+      league.save
     }
   end
 end
