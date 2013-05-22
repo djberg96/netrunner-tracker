d21505f3655a468c92b910546a0e11a7e4c2c76e
Daniel Berger <djberg96@gmail.com>
2013-05-22 10:30:59 -0600

    Updated cards model, added some validations and helper methods, and updated the cards form and show view.

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,29 +1,32 @@
 class Card < ActiveRecord::Base
-  attr_accessible :faction, :title, :kind
+  attr_accessible :faction, :title, :kind, :cost, :agenda_points, :influence
+
+  validate :agenda_has_points_set
+  validate :non_agenda_has_no_agenda_points
 
   FACTIONS = [
+    'Anarch',
+    'Criminal',
     'Haas-Bioroid',
     'Jinteki',
     'NBN',
-    'Weyland',
     'Neutral Corporation',
-    'Anarch',
-    'Criminal',
+    'Neutral Runner',
     'Shaper',
-    'Neutral Runner'
+    'Weyland'
   ]
 
   TYPES = [
     'Agenda',
     'Asset',
+    'Event',
+    'Hardware',
     'Ice',
     'Identity',
     'Operation',
-    'Upgrade',
-    'Event',
-    'Hardware',
     'Program',
     'Resource',
+    'Upgrade',
   ]
 
   validates :title,
@@ -37,4 +40,29 @@ class Card < ActiveRecord::Base
   validates :kind,
     :presence  => true,
     :inclusion => {:in => TYPES, :message => "Invalid card type"}
+
+  validates :agenda_points,
+    :inclusion => {:in => 1..3},
+    :allow_nil => true
+
+  validates :cost, :inclusion => {:in => 0..15}
+  validates :influence, :inclusion => {:in => 0..4}
+
+  def agenda_has_points_set
+    if kind == 'Agenda' && agenda_points.nil?
+      errors.add :agenda_points, "Agenda must be worth 1-3 points"
+    end
+  end
+
+  def non_agenda_has_no_agenda_points
+    if kind != 'Agenda' && agenda_points
+      errors.add :agenda_points, "Cannot set agenda points for non-agenda card"
+    end
+  end
+
+  ## Handy Methods
+
+  def agenda?
+    kind == 'Agenda'
+  end
 end
