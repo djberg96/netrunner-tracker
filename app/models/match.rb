2f0497423d4c88040f73b709cea1291958ee4520
Daniel Berger <djberg96@gmail.com>
2013-05-24 09:54:37 -0600

    Added a 'players' helper method to the Match model.

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,7 +2,15 @@ class Match < ActiveRecord::Base
   attr_accessible :date, :game_id, :name
 
   has_many :games
+
   belongs_to :tournament
 
   accepts_nested_attributes_for :games
+
+  #validates :players_switched_sides
+
+  def players
+    game = games.first
+    [game.runner_user.userid, game.corporation_user.userid]
+  end
 end
