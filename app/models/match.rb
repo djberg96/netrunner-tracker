e6e9eeb569ac40200ab1a7893346de95be377c7e
Daniel Berger <djberg96@gmail.com>
2013-05-24 09:31:29 -0600

    Added name to attr_accesible for match.

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,6 +1,8 @@
 class Match < ActiveRecord::Base
-  attr_accessible :date, :game_id
+  attr_accessible :date, :game_id, :name
 
   has_many :games
   belongs_to :tournament
+
+  accepts_nested_attributes_for :games
 end
