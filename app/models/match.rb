b2b51f0edfd32e2d4274400ca947906305c14565
Daniel Berger <djberg96@gmail.com>
2013-05-23 19:04:12 -0600

    Added relationship between match and tournament.

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,6 @@
 class Match < ActiveRecord::Base
   attr_accessible :date, :game_id
+
   has_many :games
+  belongs_to :tournament
 end
--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,3 +1,4 @@
 class Tournament < ActiveRecord::Base
   attr_accessible :created_by, :name, :num_players, :num_rounds, :winner
+  has_many :matches
 end
