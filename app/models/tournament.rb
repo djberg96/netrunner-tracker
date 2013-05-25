a0cdf9c54bdf2434de38e0292b7b6aa094865743
Daniel Berger <djberg96@gmail.com>
2013-05-25 15:24:06 -0600

    Added a has_many :through relationship from tournament to games.

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,5 +1,6 @@
 class Tournament < ActiveRecord::Base
   attr_accessible :created_by, :name, :num_players, :num_rounds, :winner
   has_many :matches
+  has_many :games, :through => :matches
   belongs_to :created_by, :class_name => 'User', :foreign_key => 'created_by'
 end
