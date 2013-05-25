f81c231ef9408048e64dbd3a452303164f0fc7ff
Daniel Berger <djberg96@gmail.com>
2013-05-25 16:03:12 -0600

    Added validation and helper method to tournament model.

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -3,4 +3,14 @@ class Tournament < ActiveRecord::Base
   has_many :matches
   has_many :games, :through => :matches
   belongs_to :created_by, :class_name => 'User', :foreign_key => 'created_by'
+
+  ## Validations
+  validates :name, :length => {:maximum => 32}
+
+  ## Handy Methods
+
+  # Returns an array of userid's associated with the tournament.
+  def players
+    self.games.map(&:players)
+  end
 end
