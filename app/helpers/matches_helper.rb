d0aaab90f66c547dcee972800d6350c8bd7c7f99
Daniel Berger <djberg96@gmail.com>
2013-06-03 10:36:07 -0600

    Added tooltips to the match form, which also had some terminology updates.

--- app/helpers/matches_helper.rb
+++ app/helpers/matches_helper.rb
@@ -1,2 +1,9 @@
 module MatchesHelper
+  def runner_agenda_tooltip
+    "The number of Agenda Points scored by the Runner."
+  end
+
+  def corp_agenda_tooltip
+    "The number of Agenda Points scored by the Corporation."
+  end
 end
