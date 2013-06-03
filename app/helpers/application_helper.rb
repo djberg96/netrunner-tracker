6fb49a679289cfa0add4cefda3e1e6b51116dee0
Daniel Berger <djberg96@gmail.com>
2013-06-03 08:55:42 -0600

    Added some tooltips for the main menu options.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,18 @@ module ApplicationHelper
     User.find(session[:user_id])
   end
 
+  def match_tooltip
+    "Use this to record a set of two games, one as the Runner and one as the Corporation.\n\nIf you want to record a single game, use the 'Record Game' option instead"
+  end
+
+  def game_tooltip
+    "Use this to record a single game.\n\nIf want to record a match (one game as Runner, one as Corporation) use the 'Record Match' option instead."
+  end
+
+  def profile_tooltip
+    "Click here to view your profile information, including game statistics."
+  end
+
   def logged_in?
     session[:user_id]
   end
