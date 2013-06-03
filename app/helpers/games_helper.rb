137aaf8cf6baadec5b4106c1cc9d6d0d4c76e915
Daniel Berger <djberg96@gmail.com>
2013-06-03 10:29:50 -0600

    Added tooltips to the game form to clarify score meaning.

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,9 @@
 module GamesHelper
+  def runner_agenda_tooltip
+    "The number of Agenda Points scored by the Runner."
+  end
+
+  def corp_agenda_tooltip
+    "The number of Agenda Points scored by the Corporation."
+  end
 end
